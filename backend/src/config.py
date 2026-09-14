0c550da06142448449faed023070c50bb1e4ef68
Dawpaw <16710770+Dawpaw@users.noreply.github.com>
2026-09-14 19:40:23 +0200

    added support for covers and extracted upload endpoint to its own router

--- backend/src/config.py
+++ backend/src/config.py
@@ -13,7 +13,7 @@ R2_ENDPOINT_URL = os.environ.get("R2_ENDPOINT_URL")
 R2_ACCESS_KEY_ID = os.environ.get("R2_ACCESS_KEY_ID") 
 R2_SECRET_ACCESS_KEY = os.environ.get("R2_SECRET_ACCESS_KEY") 
 R2_PUBLIC_ACCESS_DOMAIN = os.environ.get("R2_PUBLIC_ACCESS_DOMAIN")
-R2_BUCKET_NAME = os.environ.get("R2_BUCKET_NAME ")
+R2_BUCKET_NAME = os.environ.get("R2_BUCKET_NAME")
 
 
 def get_postgres_uri():
--- backend/src/services/image_processing.py
+++ backend/src/services/image_processing.py
@@ -1,34 +1,79 @@
-from pathlib import Path
-from uuid import uuid4
+from dataclasses import (
+    dataclass,
+)
+from pathlib import (
+    Path,
+)
+from uuid import (
+    uuid4,
+)
+
+from PIL import (
+    Image,
+)
+from src.config import (
+    IMAGES_TEMP_FOLDER,
+    R2_BUCKET_NAME,
+    s3,
+)
+from src.enums import (
+    ImageTypes,
+)
 
 
-from src.services.unit_of_work import SqlAlchemyUnitOfWork
-from src.config import IMAGES_TEMP_FOLDER, s3, R2_BUCKET_NAME
-from PIL import Image
+@dataclass
+class ImageSize:
+    width: int
+    height:int
+
 
 IMAGE_FORMAT = "webp"
-def process_image(file_name: str, uow: SqlAlchemyUnitOfWork):
+
+def save_images(file_bytes: bytes, file_name: str):
+    upload_dir = Path(IMAGES_TEMP_FOLDER)
+    upload_dir.mkdir(exist_ok=True, parents=True)
+    file_save_path = upload_dir / Path(file_name)
+    with open(file_save_path, "wb") as file_doc:
+        file_doc.write(file_bytes)  
+
+def process_image(file_bytes: bytes, file_name: str | None, image_type: ImageTypes):
+    temp_name : str = file_name or f"{uuid4()}"
+    save_images(file_bytes, temp_name)  
+    sizes_endcards: list[ImageSize] = [
+        ImageSize(1920, 1080),
+        ImageSize(1280, 720), 
+        ImageSize(640, 360)
+    ]
+
+    sizes_cover: list[ImageSize] = [ImageSize(460, 650)]
+    if(image_type == ImageTypes.COVER):
+        _process_image_internal(temp_name, sizes_cover, "covers/")
+    
+    elif(image_type == ImageTypes.ENDCARD):
+        _process_image_internal(temp_name, sizes_endcards, "endcard/")
+    
+    else:
+        print("How did you even get here?")
+    
+    
+def _process_image_internal(file_name: str, sizes: list[ImageSize], s3_folder: str):
     original_image_path = Path(IMAGES_TEMP_FOLDER) / Path(file_name)
     image = Image.open(original_image_path)
-    sizes = [
-        {"width": 1920, "height": 1080},
-        {"width": 1280, "height": 720},
-        {"width": 640, "height": 360},
-    ]
-    image_names = [f"{uuid4()}_{k}.{IMAGE_FORMAT}" for k in ("l", "m", "s")]
+    # TODO maybe try to extract the image name creation and return that in the api
+    if(len(sizes) == 1):
+        image_names = [f"{uuid4()}_l.{IMAGE_FORMAT}"]
+    else:
+        image_names = [f"{uuid4()}_{k}.{IMAGE_FORMAT}" for k in ("l", "m", "s")]
     images_dests: list[Path] = [Path(IMAGES_TEMP_FOLDER) / Path(image_name) for image_name in image_names]
 
-    for image_dest, size in zip(images_dests,sizes):
+    for image_dest, size in zip(images_dests, sizes):
         image_copy = image.copy()
-        image_copy.thumbnail((size["width"], size["height"]))
+        image_copy.thumbnail((size.width, size.height))
         image_copy.info.clear()
         image_copy.save(image_dest, IMAGE_FORMAT)
     
-    # TODO upload the images to object storage
     for image_dest, image_name in zip(images_dests, image_names):
-        upload_images_s3(image_dest, image_name)
-
-    # TODO get the images urls and save them into the database
+        upload_images_s3(image_dest, f"{s3_folder}{image_name}")
 
     # Delete all saved images
     for img in images_dests:
